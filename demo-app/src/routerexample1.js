26dcb105d4e96998b472d3e213878622c7e7951e
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-05-11 19:20:07 -0700

    WEB503 DEMO M5 W2 D2 initiate

---
+++ demo-app/src/routerexample1.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/users" element={<Users />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </Router>      
+  );
+
+  function Home() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <p>This is home</p>
+      </div>
+    )
+  }
+
+  function About() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>This is about page</p>
+      </div>
+    )
+  }
+
+  function Users() {
+    return (
+      <div>
+        <h2>Users</h2>
+        <p>This is users page</p>
+      </div>
+    )
+  }
+}
